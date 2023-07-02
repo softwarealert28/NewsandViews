d74f40e92fd8ae98fcbacbb54080ed61f5079f2d
Amrit Singh <amrit4qa@gmail.com>
2023-07-02 14:28:30 -0400

    added your news section

--- src/dailyNews/Testerclass.java
+++ src/dailyNews/Testerclass.java
@@ -5,6 +5,7 @@ public class Testerclass {
 	public static void main(String[] args) {
 		System.out.println("it works");
 		// this is comment line
+		System.out.println("Added your news sections");
 
 	}
 
